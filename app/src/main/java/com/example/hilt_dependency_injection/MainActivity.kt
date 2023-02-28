c674aff6a7aa11b95a303f56eeb07f251e376a4f
Elisabeth Farias <elisabethf@ciandt.com>
2023-02-28 13:42:49 -0300

    Added hilt

--- app/src/main/java/com/example/hilt_dependency_injection/MainActivity.kt
+++ app/src/main/java/com/example/hilt_dependency_injection/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.hilt_dependency_injection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val model: HelloViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.hello).text = model.text
     }
+
+}
+
+class HelloViewModel @Inject constructor() : ViewModel() {
+    val text = "Hello from ViewModel"
 }
